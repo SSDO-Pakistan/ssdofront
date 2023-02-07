eb2799cf44ada2dc7c52a38bf97c7ec8a20a48d7
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-07 23:39:27 +0500

    change profile area heading

--- pages/about-us/[path].js
+++ pages/about-us/[path].js
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 import { API_URL } from "@/config/index";
 import TeamModel from "../../components/TeamModal";
 import Image from 'next/image'
+import {useRouter} from 'next/router';
+
 const Profile = ({ data }) => {
+  const router = useRouter()
+ if (router.query.path == 'team')
+ {
+    var heading = 'Our Team'
+ }
+ else if(router.query.path=='directors'){
+   var heading = 'Board of Directors'
+ }
   const [showProfile, setshowProfile] = useState({});
   console.log(showProfile);
   const [modalShow, setmodalShow] = useState(false);
@@ -12,10 +22,11 @@ const Profile = ({ data }) => {
       className="container content-space-1 p-4 mt-20"
       style={{ overflowX: "hidden" }}
     >
-      <div className="w-lg-100 text-center mx-lg-auto mb-5 mb-sm-7 mb-lg-10">
-        <h2>Our leadership</h2>
-      </div>
-
+       <div className="block-title-6 text-center ">
+       <h4 className="h5 border-primary">
+          <span className="bg-primary text-white fw-bolder ">{heading}</span>
+        </h4>
+        </div>
       <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
         {data.Profiles.data.map((profile) => {
           return (
@@ -28,18 +39,16 @@ const Profile = ({ data }) => {
               }}
              key ={profile.attributes.id}>
              <div className="card shadow-sm p-3 mb-5 bg-white rounded" style={{width:"16rem"}}>
-                <Image
-                
+                <Image     
                   width={225}
                   height={225}
-                  src={
-                  
+                  src={   
                     profile.attributes.Profile[0].image.data[0].attributes.url
                   }
                   alt="Image Description"
                 />
                  <div className="card-body">
-                <h5 className="mb-1 text-center fw-bolder">{profile.attributes.name}</h5>
+                <h5 className="mb-1 text-center fw-bold">{profile.attributes.name}</h5>
                 <span className="d-block text-center" style={{color:"grey"}}>
                   {profile.attributes.Profile[0].title}
                 </span>
@@ -48,74 +57,6 @@ const Profile = ({ data }) => {
             </div>
           );
         })}
-
-        {/* <div class="col mb-10">
-          <div class="w-sm-65 text-center mx-auto">
-            <img
-              class="img-fluid rounded-3 mb-4"
-              src="../assets/img/350x350/img5.jpg"
-              alt="Image Description"
-            />
-            <h5 class="mb-1">Amil Evara</h5>
-            <span class="d-block">UI/UX Designer</span>
-          </div>
-        </div> */}
-        {/* 
-    <div class="col mb-10">
-      <div class="w-sm-65 text-center mx-auto">
-        <img class="img-fluid rounded-3 mb-4" src="../assets/img/350x350/img9.jpg" alt="Image Description">
-        <h5 class="mb-1">Ebele Egbuna</h5>
-        <span class="d-block">Support Consultant</span>
-      </div>
-    </div>
-
-    <div class="col mb-10">
-      <div class="w-sm-65 text-center mx-auto">
-        <img class="img-fluid rounded-3 mb-4" src="../assets/img/350x350/img7.jpg" alt="Image Description">
-        <h5 class="mb-1">Maria Powers</h5>
-        <span class="d-block">Director of sales</span>
-      </div>
-    </div>
-
-    <div class="col mb-10">
-      <div class="w-sm-65 text-center mx-auto">
-        <img class="img-fluid rounded-3 mb-4" src="../assets/img/350x350/img8.jpg" alt="Image Description">
-        <h5 class="mb-1">Delia Pawelke</h5>
-        <span class="d-block">Front-end Developer</span>
-      </div>
-    </div>
-
-    <div class="col mb-10">
-      <div class="w-sm-65 text-center mx-auto">
-        <img class="img-fluid rounded-3 mb-4" src="../assets/img/350x350/img6.jpg" alt="Image Description">
-        <h5 class="mb-1">Tom Lowry</h5>
-        <span class="d-block">UI/UX Designer</span>
-      </div>
-    </div>
-
-    <div class="col mb-10">
-      <div class="w-sm-65 text-center mx-auto">
-        <img class="img-fluid rounded-3 mb-4" src="../assets/img/350x350/img1.jpg" alt="Image Description">
-        <h5 class="mb-1">Louise Donadieu</h5>
-        <span class="d-block">Support Consultant</span>
-      </div>
-    </div>
-
-    <div class="col mb-10">
-      <div class="w-sm-65 text-center mx-auto">
-        <img class="img-fluid rounded-3 mb-4" src="../assets/img/350x350/img2.jpg" alt="Image Description">
-        <h5 class="mb-1">Jeff Fisher</h5>
-        <span class="d-block">Project Manager</span>
-      </div>
-    </div>
-
-    <div class="col mb-10">
-      <div class="w-sm-65 text-center mx-auto">
-        <img class="img-fluid rounded-3 mb-4" src="../assets/img/350x350/img3.jpg" alt="Image Description">
-        <h5 class="mb-1">Sophia Harrington</h5>
-        <span class="d-block">Project Manager</span>
-      </div>
-    </div> */}
       </div>
       <TeamModel
         show={modalShow}
