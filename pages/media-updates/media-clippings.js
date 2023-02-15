28f5925081e404a29d2aa1fbf63d3eb210c3ad69
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-16 00:48:21 +0500

    update code in media-clippings

--- pages/media-updates/media-clippings.js
+++ pages/media-updates/media-clippings.js
@@ -1,11 +1,64 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback,useEffect } from "react";
 import ReactPaginate from "react-paginate";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
-function Photos(props) {
-  let limit = 30
-  const [items, setItems] = useState(props.mydata);
-  let pageCount=Math.ceil(props.total/limit)
+function Photos() {
+
+  const [items, setItems] = useState([]);
+
+  const [pageCount, setpageCount] = useState(0);
+  let limit = 20;
+
+  useEffect(() => {
+    const getMedia = async () => {
+      const res = await fetch(
+        `https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=${limit}`
+        // `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
+      );
+      const data = await res.json();
+      const total = data.meta.pagination.total;
+      let mydata = new Array;
+      data.data?.map((clip) => {
+                  mydata.push( {"src":clip.attributes.image.data.attributes.url,
+                  "width":clip.attributes.image.data.attributes.width,
+                  "height":clip.attributes.image.data.attributes.height});
+              });
+   
+      setpageCount(Math.ceil(total / limit));
+      // console.log(Math.ceil(total/12));
+      setItems(mydata);
+    };
+
+     getMedia();
+  }, [limit]);
+  
+
+
+  const fetchMedia = async (currentPage) => {
+    const res = await fetch(
+      //`http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
+      `https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
+    );
+    const data = await res.json();
+    let mydata = new Array;
+    data.data?.map((clip) => {
+                mydata.push( {"src":clip.attributes.image.data.attributes.url,
+                "width":clip.attributes.image.data.attributes.width,
+                "height":clip.attributes.image.data.attributes.height});
+            });
+      return mydata;
+  };
+  const handlePageClick = async (data) => {
+    
+
+    let currentPage = data.selected + 1;
+    console.log(currentPage);
+    const mediaClips = await fetchMedia(currentPage);
+
+    setItems(mediaClips);
+    // scroll to the top
+    //window.scrollTo(0, 0)
+  };
 
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
@@ -19,32 +72,6 @@ function Photos(props) {
     setCurrentImage(0);
     setViewerIsOpen(false);
   };
-  const fetchComments = async (currentPage) => {
-    const res = await fetch(
-      //`http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
-      `https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
-    );
-    const data = await res.json();
-    let mydata = new Array;
-    data.data?.map((clip) => {
-                mydata.push( {"src":clip.attributes.image.data.attributes.url,
-                "width":clip.attributes.image.data.attributes.width,
-                "height":clip.attributes.image.data.attributes.height});
-            });
-            return mydata;
-  };
-  const handlePageClick = async (data) => {
-    console.log(data.selected);
-
-    let currentPage = data.selected + 1;
-
-    const commentsFormServer = await fetchComments(currentPage);
-
-    setItems(commentsFormServer);
-    // scroll to the top
-    //window.scrollTo(0, 0)
-  };
-
   return (
     <div className="row p-4 mt-20">
     <div className="col-sm-12">
@@ -94,26 +121,26 @@ function Photos(props) {
   );
 }
 export default Photos
-export async function getStaticProps() {
-    const res = await
-fetch('https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=10')
+// export async function getStaticProps() {
+//     const res = await
+// fetch('https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[start]=1&pagination[limit]=10')
 
-    const photos = await res.json()
-    const total = photos.meta.pagination.total;
-    let mydata = new Array;
-    photos.data?.map((clip) => {
-                mydata.push( {"src":clip.attributes.image.data.attributes.url,
-                "width":clip.attributes.image.data.attributes.width,
-                "height":clip.attributes.image.data.attributes.height});
-            });
-    return {
-      props: {
-        mydata,
-        total
-      },
-      // Next.js will attempt to re-generate the page:
-      // - When a request comes in
-      // - At most once every 10 seconds
-      revalidate: 10, // In seconds
-    }
-  }
+//     const photos = await res.json()
+//     const total = photos.meta.pagination.total;
+//     let mydata = new Array;
+//     photos.data?.map((clip) => {
+//                 mydata.push( {"src":clip.attributes.image.data.attributes.url,
+//                 "width":clip.attributes.image.data.attributes.width,
+//                 "height":clip.attributes.image.data.attributes.height});
+//             });
+//     return {
+//       props: {
+//         mydata,
+//         total
+//       },
+//       // Next.js will attempt to re-generate the page:
+//       // - When a request comes in
+//       // - At most once every 10 seconds
+//       revalidate: 10, // In seconds
+//     }
+//   }
