bd81dcb5b2444a96444a9ec8f560f00dda9006ae
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-17 23:30:35 +0500

    change phtogallery class to photo album

--- pages/media-updates/media-clippings.js
+++ pages/media-updates/media-clippings.js
@@ -1,83 +1,73 @@
-import React, { useState, useCallback,useEffect } from "react";
+import { PhotoAlbum, RenderPhoto } from "react-photo-album";
+import React, { useState, useCallback } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 import ReactPaginate from "react-paginate";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-function Photos() {
 
-  const [items, setItems] = useState([]);
+function Photosalbum(props) {
+  let limit = 10
+  const [items, setItems] = useState(props.mydata);
+  let pageCount = Math.ceil(props.total / limit)
+  const [index, setIndex] = useState(-1);
 
-  const [pageCount, setpageCount] = useState(0);
-  let limit = 20;
+  const renderPhoto = ({ layout, layoutOptions, imageProps: { alt,
+    style, ...restImageProps },
+    photo: { src, tags } }) => (
+    <div className="card shadow-sm" >
+      <div class="card-body">
+        <img alt={alt} style={{ ...style, width: "100%", padding: 0 }}
+          {...restImageProps} />
+        <div>
+          {tags && (
+            <ul className="photo-tags">
+              {tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 
-  useEffect(() => {
-    const getMedia = async () => {
-      const res = await fetch(
-        `https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=${limit}`
-        // `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
-      );
-      const data = await res.json();
-      console.log(data)
-      const total = data.meta.pagination.total;
-      let mydata = new Array;
-      data.data?.map((clip) => {
-                  mydata.push( {"src":clip.attributes.image.data.attributes.url,
-                 "srcSet": clip.attributes.image.data.attributes.formats.small.url
-                   ,
-                  "sizes": ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-                  "width":clip.attributes.image.data.attributes.width,
-                  "height":clip.attributes.image.data.attributes.height});
-              });
-   
-      setpageCount(Math.ceil(total / limit));
-   
-      setItems(mydata);
-    };
-
-     getMedia();
-  }, [limit]);
-  
-
-
-  const fetchMedia = async (currentPage) => {
+  const fetchPhotos = async (currentPage) => {
     const res = await fetch(
-      //`http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
       `https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
     );
     const data = await res.json();
     let mydata = new Array;
     data.data?.map((clip) => {
-                mydata.push( {"src":clip.attributes.image.data.attributes.url,
-                 "srcSet": clip.attributes.image.data.attributes.formats.small.url,        
-                 "sizes": ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-                "width":clip.attributes.image.data.attributes.width,
-                "height":clip.attributes.image.data.attributes.height});
-            });
-      return mydata;
+      mydata.push({
+        "src": clip.attributes.image.data.attributes.url,
+        "width": clip.attributes.image.data.attributes.width,
+        "height": clip.attributes.image.data.attributes.height,
+        "images": [
+          {
+            "src":
+              clip.attributes.image.data.attributes.formats.small.url
+          },
+          {
+            "src":
+              clip.attributes.image.data.attributes.formats.thumbnail.url
+          }
+        ]
+      });
+    });
+    return mydata;
   };
   const handlePageClick = async (data) => {
-    
-
     let currentPage = data.selected + 1;
-    console.log(currentPage);
-    const mediaClips = await fetchMedia(currentPage);
-
-    setItems(mediaClips);
-    // scroll to the top
-    //window.scrollTo(0, 0)
+    const commentsFormServer = await fetchPhotos(currentPage);
+    setItems(commentsFormServer);
   };
 
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
 
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
   return (
     <div className="row p-4 mt-20">
     <div className="col-sm-12">
@@ -86,22 +76,23 @@ function Photos() {
           <span className="bg-primary text-white">Media Clippings</span>
         </h4>
       </div>
-      <Gallery photos={items} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={items.map(x => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
-              <br></br>
+      <PhotoAlbum
+        layout="rows"
+        photos={items}
+        spacing={20}
+        padding={20}
+        targetRowHeight={200}
+        renderPhoto={renderPhoto}
+        onClick={({ photo: { src }, index }) => setIndex(index)}
+      />
+      <Lightbox
+        slides={items}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
       <ReactPaginate
         previousLabel={"previous"}
         nextLabel={"next"}
@@ -122,31 +113,36 @@ function Photos() {
         activeClassName={"active"}
       />
     </div>
-    </div>
-
+</div>
   );
 }
-export default Photos
-// export async function getStaticProps() {
-//     const res = await
-// fetch('https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[start]=1&pagination[limit]=10')
+export default Photosalbum
+export async function getStaticProps() {
+  const res = await
+    fetch('https://strapi-production-9f68.up.railway.app/api/media-clipings?populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=10')
 
-//     const photos = await res.json()
-//     const total = photos.meta.pagination.total;
-//     let mydata = new Array;
-//     photos.data?.map((clip) => {
-//                 mydata.push( {"src":clip.attributes.image.data.attributes.url,
-//                 "width":clip.attributes.image.data.attributes.width,
-//                 "height":clip.attributes.image.data.attributes.height});
-//             });
-//     return {
-//       props: {
-//         mydata,
-//         total
-//       },
-//       // Next.js will attempt to re-generate the page:
-//       // - When a request comes in
-//       // - At most once every 10 seconds
-//       revalidate: 10, // In seconds
-//     }
-//   }
+  const photos = await res.json()
+  const total = photos.meta.pagination.total;
+  let mydata = new Array;
+  photos.data?.map((clip) => {
+    mydata.push({
+      "src": clip.attributes.image.data.attributes.url,
+      "width": clip.attributes.image.data.attributes.width,
+      "height": clip.attributes.image.data.attributes.height,
+      "images": [
+        { "src": clip.attributes.image.data.attributes.formats.small.url },
+        { "src": clip.attributes.image.data.attributes.formats.thumbnail.url }
+      ]
+    });
+  });
+  return {
+    props: {
+      mydata,
+      total
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  }
+}
