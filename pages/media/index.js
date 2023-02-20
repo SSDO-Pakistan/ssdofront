db78de61e11ca6630e42b1f401e2fcdf4a3a9240
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 15:06:11 +0500

    changes in media divs

--- pages/media/index.js
+++ pages/media/index.js
@@ -2,26 +2,30 @@ import React from 'react'
 import Link from "next/link";
 
 function Media() {
-  return (
-    <div className="container mt-5 mb-5">
-    <div className="card" style={{width: "18rem", height:"20rem",backgroundColor:"#C84869" ,float:"left" }}>
-        <div className="card-body">
-        <Link href={`media/media-clippinigs`}> 
-            <h5 className="card-title" style={{color:"white",fontWeight:"bolder"}}>Videos</h5>
-        </Link> 
-        </div>
-      
-    </div>
-    <div className="card" style={{width: "18rem", height:"20rem", backgroundColor:"#C84869"}}>
-        <div className="card-body">
+    return (
+        <div class="row">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+                <div className="card" style={{ width: "18rem", height: "20rem", backgroundColor: "#C84869", float: "left", }}>
+                    <div className="card-body">
+                        <Link href={`media/media-clippings`}>
+                            <h5 className="card-title text-center" style={{ color: "white", fontWeight: "bolder" }}>Videos</h5>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div className="card" style={{ width: "18rem", height: "20rem", backgroundColor: "#C84869", marginLeft: "2%" }}>
+                    <div className="card-body">
 
-          <Link href={`media/media-clippinigs`}> 
-           <h5 className="card-title" style={{color:"white",fontWeight:"bolder"}}>Media Clippings</h5>
-          </Link>
+                        <Link href={`media/media-clippinigs`}>
+                            <h5 className="card-title text-center" style={{ color: "white", fontWeight: "bolder" }}>Media Clippings</h5>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-  )
+
+    )
 }
 
 export default Media
