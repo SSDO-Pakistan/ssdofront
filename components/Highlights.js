990c6a7c625222dc9a569ddf4d794fd797b2f63f
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 11:17:27 +0500

    changes link in hightlights

--- components/Highlights.js
+++ components/Highlights.js
@@ -33,9 +33,11 @@ const Highlights = ({ Highlights }) => {
                   </div>
                   <div className="col-sm-6 col-md-12 col-lg-6">
                     <div className="card-body pt-3 pt-sm-0 pt-md-3 pt-lg-0">
+                      <Link href={`highlights/${highlight.attributes.slug}`}>
                       <h3 className="card-title h2 h3-sm h2-md">
                         {highlight.attributes.title}
                       </h3>
+                      </Link>
                       <div className="card-text mb-2 text-muted small">
                         {/* <span className="d-none d-sm-inline me-1">
                               <a className="fw-bold" href="#">
