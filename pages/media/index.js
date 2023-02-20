3c49bf2b08266710c1d72b33586ef52101eb6b20
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 15:21:26 +0500

    changes in the media page div style

--- pages/media/index.js
+++ pages/media/index.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 function Media() {
     return (
-        <div class="row">
-            <div class="col-sm-6 mb-3 mb-sm-0">
-                <div className="card" style={{ width: "18rem", height: "20rem", backgroundColor: "#C84869" }}>
+        <div class="row mt-5 mb-5">
+            <div class="col-sm-6 mb-3 mb-sm-0 ">
+                <div className="card" style={{ width: "18rem", height: "18rem", backgroundColor: "#C84869" }}>
                     <div className="card-body">
                         <Link href={`media/media-clippings`}>
                             <h5 className="card-title text-center" style={{ color: "white", fontWeight: "bolder" }}>Videos</h5>
@@ -14,7 +14,7 @@ function Media() {
                 </div>
             </div>
             <div class="col-sm-6">
-                <div className="card" style={{ width: "18rem", height: "20rem", backgroundColor: "#C84869"}}>
+                <div className="card" style={{ width: "18rem", height: "18rem", backgroundColor: "#C84869"}}>
                     <div className="card-body">
 
                         <Link href={`media/media-clippinigs`}>
