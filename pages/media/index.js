ef13212e4ef101531e8d374fef39f809bf097297
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 15:00:03 +0500

    changes in static style of media page

--- pages/media/index.js
+++ pages/media/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import Link from "next/link";
 
 function Media() {
   return (
-    <div className="container">
-    <div className="card" style={{width: "18rem",backgroundColor:"#C84869" ,float:"left" ,color:"white"}}>
+    <div className="container mt-5 mb-5">
+    <div className="card" style={{width: "18rem", height:"20rem",backgroundColor:"#C84869" ,float:"left" }}>
         <div className="card-body">
-            <h5 className="card-title">Videos</h5>
+        <Link href={`media/media-clippinigs`}> 
+            <h5 className="card-title" style={{color:"white",fontWeight:"bolder"}}>Videos</h5>
+        </Link> 
         </div>
+      
     </div>
-    <div className="card" style={{width: "18rem", backgroundColor:"#C84869",color:"white"}}>
+    <div className="card" style={{width: "18rem", height:"20rem", backgroundColor:"#C84869"}}>
         <div className="card-body">
-            <h5 className="card-title">Media Clippings</h5>
+
+          <Link href={`media/media-clippinigs`}> 
+           <h5 className="card-title" style={{color:"white",fontWeight:"bolder"}}>Media Clippings</h5>
+          </Link>
         </div>
     </div>
     </div>
