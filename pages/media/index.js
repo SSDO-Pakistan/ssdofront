376ab096cf74c9015d66d740a54269926b65b363
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 14:38:16 +0500

    static page name change1

--- pages/media/index.js
+++ pages/media/index.js
@@ -2,21 +2,8 @@ import React from 'react'
 
 function index() {
   return (
-    <div className='container'>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Videos</h5>
-        </div>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Media Clippings</h5>
-        </div>
-    </div>
-</div>
+    <div>index</div>
   )
 }
 
 export default index
-
-
