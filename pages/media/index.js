041b6e23099ea98bce9b9c099f73e104f985eff9
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 14:32:17 +0500

    static media page

--- pages/media/index.js
+++ pages/media/index.js
@@ -1,19 +1,9 @@
 import React from 'react'
 
-function Media() {
-    return (
-        <div className='container'>
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Videos</h5>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Media Clippings</h5>
-                </div>
-            </div>
-        </div>
-    )
+function index() {
+  return (
+    <div>index</div>
+  )
 }
-export default Media
+
+export default index
