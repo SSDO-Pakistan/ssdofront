471245c1d474e4af0712a76fb1b80e4e99a2c7d7
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 14:41:59 +0500

    final changes to media static page

--- pages/media/index.js
+++ pages/media/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-function index() {
+function Media() {
   return (
-    <div>index</div>
+    <div className="container">
+    <div className="card" style={{width: "18rem"}}>
+        <div className="card-body">
+            <h5 className="card-title">Videos</h5>
+        </div>
+    </div>
+    <div className="card" style={{width: "18rem"}}>
+        <div className="card-body">
+            <h5 className="card-title">Media Clippings</h5>
+        </div>
+    </div>
+    </div>
   )
 }
 
-export default index
+export default Media
