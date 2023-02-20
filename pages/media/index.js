2430bd3fbc2da99d608b74eac54a7a5a011d35b6
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-02-20 14:52:01 +0500

    media index page to static one

--- pages/media/index.js
+++ pages/media/index.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Media() {
   return (
     <div className="container">
-    <div className="card" style={{width: "18rem"}}>
+    <div className="card" style={{width: "18rem",backgroundColor:"#C84869" ,float:"left" ,color:"white"}}>
         <div className="card-body">
             <h5 className="card-title">Videos</h5>
         </div>
     </div>
-    <div className="card" style={{width: "18rem"}}>
+    <div className="card" style={{width: "18rem", backgroundColor:"#C84869",color:"white"}}>
         <div className="card-body">
             <h5 className="card-title">Media Clippings</h5>
         </div>
