0f311311d705a006aa7690e9a704364b23bb32d2
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-03-11 18:43:04 +0500

    change publication file to static

--- pages/publications.js
+++ pages/publications.js
@@ -1,5 +1,5 @@
 import { PhotoAlbum, RenderPhoto } from "react-photo-album";
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
@@ -8,61 +8,17 @@ import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import ReactPaginate from "react-paginate";
-import { API_URL } from "@/config/index";
-import { useRouter } from "next/router";
-import Link from "next/link";
 import Layout from "@/components/Layout";
-function Publications() {
-
-  //console.log("reports", reports);
+import { API_URL } from "@/config/index";
+import Link from "next/link";
+function Publications(props) {
   let limit = 10
-  const [items, setItems] = useState([]);
-  const [pageCount, setpageCount] = useState(0);
-  
-  const [index, setIndex] = useState(-1);
-  useEffect(() => {
-    
-    const getPublications = async () => {
-      const res = await fetch(
-        `${API_URL}/api/publications?&populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=${limit}`
-        // `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
-      );
-      const data = await res.json();
-      console.log("pub", data);
-      const total = data.meta.pagination.total;
-      setpageCount(Math.ceil(total / limit));
-      let mydata = new Array;
-      let myFileSrc
-      data.data?.map((clip) => {
-       
-      if( !clip.attributes.File)
-      {
-        myFileSrc = "No File"
-      }
-      else
-      {
-        myFileSrc=clip.attributes.File.data.attributes.url
-      }
-        mydata.push( clip.attributes.File.data.attributes.url && { 
-           
-          "file": myFileSrc,
-          "src": clip.attributes.cover.data.attributes.url,
-          "width": clip.attributes.cover.data.attributes.width,
-          "height": clip.attributes.cover.data.attributes.height,
-          "images": [ 
-            {
-              "src": clip.attributes.cover.data.attributes.formats.thumbnail.url,
-            
-            }
-          ]
-        });
-      });
-      setItems(mydata);
-    };
 
-    getPublications();
-  }, [limit]);
-  //console.log("items", items);
+  const [items, setItems] = useState(props.mydata);
+   // console.log("my items", items);
+  let pageCount = Math.ceil(props.total / limit)
+  const [index, setIndex] = useState(-1);
+
   const renderPhoto = ({ layout, layoutOptions, imageProps: { alt,
     style, ...restImageProps },
     photo: { file, tags } }) => (
@@ -70,24 +26,33 @@ function Publications() {
       <div class="card-body">
         <img alt={alt} style={{ ...style, width: "100%", padding: 0 }}
           {...restImageProps} />
-        <h5 className="card-title text-center mt-2">
+          <h5 className="card-title text-center mt-2">
           {file && (   
-           <Link href={file}><span className="badge  bg-danger text-light">Download</span></Link>
+           <Link href={file} target="_blank"><span className="badge  bg-danger text-light">Download</span></Link>
           )}
         </h5>
+        <div>
+          {tags && (
+            <ul className="photo-tags">
+              {tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
 
-  const fetchPublications = async (currentPage) => {
+  const fetchPhotos = async (currentPage) => {
     const res = await fetch(
-        `${API_URL}/api/publications?&populate=*&sort=createdAt:desc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
+      `${API_URL}/api/publications?populate=*&sort=createdAt:desc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
     );
     const data = await res.json();
     let mydata = new Array;
     let myFileSrc=""
     data.data?.map((clip) => {
-        if( clip.attributes.File)
+        if( !clip.attributes.File)
         {
           myFileSrc = "No File"
         }
@@ -95,13 +60,16 @@ function Publications() {
         {
           myFileSrc=clip.attributes.File.data.attributes.url
         }
-      mydata.push( {
+      mydata.push({
         "file": myFileSrc,
         "src": clip.attributes.cover.data.attributes.url,
         "width": clip.attributes.cover.data.attributes.width,
         "height": clip.attributes.cover.data.attributes.height,
         "images": [
-       
+          {
+            "src":
+              clip.attributes.cover.data.attributes.url
+          },
           {
             "src":
               clip.attributes.cover.data.attributes.formats.thumbnail.url
@@ -111,16 +79,13 @@ function Publications() {
     });
     return mydata;
   };
-    const handlePageClick = async (data) => {
+  const handlePageClick = async (data) => {
     let currentPage = data.selected + 1;
-    const commentsFormServer = await fetchPublications(currentPage);
+    const commentsFormServer = await fetchPhotos(currentPage);
     setItems(commentsFormServer);
   };
-
-
-
   return (
-    <Layout title="Publications">
+    <Layout title="Media Clippings">
     <div className="row p-4 mt-20">
     <div className="col-sm-12">
       <div className="block-title-6 text-center">
@@ -154,7 +119,7 @@ function Publications() {
         marginPagesDisplayed={2}
         pageRangeDisplayed={3}
         onPageChange={handlePageClick}
-        containerClassName={"pagination justify-content-center mt-5"}
+        containerClassName={"pagination justify-content-center mt-2"}
         pageClassName={"page-item"}
         pageLinkClassName={"page-link"}
         previousClassName={"page-item"}
@@ -171,4 +136,42 @@ function Publications() {
   );
 }
 export default Publications
+export async function getStaticProps() {
+  const res = await
+    fetch( `${API_URL}/api/publications?populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=10`)
 
+  const photos = await res.json()
+  const total = photos.meta.pagination.total;
+  let mydata = new Array;
+  let myFileSrc
+  photos.data?.map((clip) => {
+    if( !clip.attributes.File)
+    {
+      myFileSrc = "No File"
+    }
+    else
+    {
+      myFileSrc=clip.attributes.File.data.attributes.url
+    }
+    mydata.push({
+      "file": myFileSrc,
+      "src": clip.attributes.cover.data.attributes.url,
+      "width": clip.attributes.cover.data.attributes.width,
+      "height": clip.attributes.cover.data.attributes.height,
+      "images": [
+        { "src": clip.attributes.cover.data.attributes.url },
+        { "src": clip.attributes.cover.data.attributes.formats.thumbnail.url }
+      ]
+    });
+  });
+  return {
+    props: {
+      mydata,
+      total
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  }
+}
