b9630e91ac772607b389eea38ca14bba3f0b3220
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-03-13 00:25:54 +0500

    Added publications names

--- pages/publications.js
+++ pages/publications.js
@@ -1,5 +1,6 @@
 import { PhotoAlbum, RenderPhoto } from "react-photo-album";
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useRef } from "react";
+
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
@@ -11,52 +12,81 @@ import ReactPaginate from "react-paginate";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import Link from "next/link";
-function Publications(props) {
-  let limit = 10
 
+function Publications(props) {
+  let limit = 10;
+  //used for tooltip
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+  //============
   const [items, setItems] = useState(props.mydata);
-   // console.log("my items", items);
-  let pageCount = Math.ceil(props.total / limit)
+  // console.log("my items", items);
+  let pageCount = Math.ceil(props.total / limit);
   const [index, setIndex] = useState(-1);
-//rendering the container
-const renderContainer = ({ containerProps, children, containerRef }) => (
-  <div className="shadow-sm"
+  //rendering the container
+  const renderContainer = ({ containerProps, children, containerRef }) => (
+    <div
+      className="shadow-sm"
       style={{
-          border: "2px solid #eee",
-          borderRadius: "10px",
-          padding: "20px",
-          background:"beige"
-          
+        border: "2px solid #eee",
+        borderRadius: "10px",
+        padding: "20px",
+        background: "beige",
       }}
-  >
+    >
       <div ref={containerRef} {...containerProps}>
-          {children}
+        {children}
       </div>
-  </div>
-);
-//rendering the photo
-  const renderPhoto = ({ layout, layoutOptions, imageProps: { alt,
-    style, ...restImageProps },
-    photo: { file, tags } }) => (
-    <div className="card shadow" >
-      <div class="card-body">
-        <img alt={alt} style={{ ...style, width: "100%", padding: 0 }}
-          {...restImageProps} />
-          <h5 className="card-title text-center mt-2">
-          {file && (   
-           <Link href={file} target="_blank"><span className="badge  bg-danger text-light">Download</span></Link>
-          )}
-        </h5>
-        <div>
-          {tags && (
-            <ul className="photo-tags">
-              {tags.map((tag) => (
-                <li key={tag}>{tag}</li>
-              ))}
-            </ul>
-          )}
-        </div>
+    </div>
+  );
+  //rendering the photo
+  const renderPhoto = ({
+    layout,
+    layoutOptions,
+    imageProps: { alt, style, ...restImageProps },
+    photo: { file, tags, title },
+  }) => (
+    <div
+      className="shadow-sm"
+      style={{
+        border: "2px solid #eee",
+        borderRadius: "4px",
+        boxSizing: "content-box",
+        alignItems: "center",
+        width: style?.width,
+        padding: `${layoutOptions.padding - 2}px`,
+        paddingBottom: 0,
+        backgroundColor: "white",
+      }}
+    >
+      <img
+        alt={alt}
+        style={{ ...style, width: "100%", padding: 0 }}
+        {...restImageProps}
+      />
+
+      <div
+        style={{
+          paddingTop: "8px",
+          paddingBottom: "8px",
+          overflow: "visible",
+          textAlign: "left",
+          fontSize: "13px",
+          overflowWrap: "break-word",
+          fontWeight: "bolder",
+          color: "#545E63",
+          // inlineSize: "350px"
+        }}
+      >
+        {title.length > 50 ? title.slice(0, 50) + "..." : title}
       </div>
+      <h5 className="text-center mt-2">
+        {file && (
+          <Link href={file} target="_blank">
+            <span className="badge  bg-danger text-light">Download</span>
+          </Link>
+        )}
+      </h5>
     </div>
   );
 
@@ -65,32 +95,28 @@ const renderContainer = ({ containerProps, children, containerRef }) => (
       `${API_URL}/api/publications?populate=*&sort=createdAt:desc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
     );
     const data = await res.json();
-    let mydata = new Array;
-    let myFileSrc=""
+    let mydata = new Array();
+    let myFileSrc = "";
     data.data?.map((clip) => {
-        if( !clip.attributes.File)
-        {
-          myFileSrc = "No File"
-        }
-        else
-        {
-          myFileSrc=clip.attributes.File.data.attributes.url
-        }
+      if (!clip.attributes.File) {
+        myFileSrc = "No File";
+      } else {
+        myFileSrc = clip.attributes.File.data.attributes.url;
+      }
       mydata.push({
-        "file": myFileSrc,
-        "src": clip.attributes.cover.data.attributes.url,
-        "width": clip.attributes.cover.data.attributes.width,
-        "height": clip.attributes.cover.data.attributes.height,
-        "images": [
+        title: clip.attributes.title,
+        file: myFileSrc,
+        src: clip.attributes.cover.data.attributes.url,
+        width: clip.attributes.cover.data.attributes.width,
+        height: clip.attributes.cover.data.attributes.height,
+        images: [
           {
-            "src":
-              clip.attributes.cover.data.attributes.url
+            src: clip.attributes.cover.data.attributes.url,
           },
           {
-            "src":
-              clip.attributes.cover.data.attributes.formats.thumbnail.url
-          }
-        ]
+            src: clip.attributes.cover.data.attributes.formats.thumbnail.url,
+          },
+        ],
       });
     });
     return mydata;
@@ -102,93 +128,92 @@ const renderContainer = ({ containerProps, children, containerRef }) => (
   };
   return (
     <Layout title="Media Clippings">
-    <div className="row p-4 mt-20">
-    <div className="col-sm-12">
-      <div className="block-title-6 text-center">
-        <h4 className="h5 border-primary">
-          <span className="bg-primary text-white">Publications</span>
-        </h4>
+      <div className="row p-4 mt-20">
+        <div className="col-sm-12">
+          <div className="block-title-6 text-center">
+            <h4 className="h5 border-primary">
+              <span className="bg-primary text-white">Publications</span>
+            </h4>
+          </div>
+          <PhotoAlbum
+            layout="rows"
+            photos={items}
+            containerWidth={900}
+            spacing={20}
+            padding={20}
+            targetRowHeight={200}
+            renderContainer={renderContainer}
+            renderPhoto={renderPhoto}
+            onClick={({ photo: { File }, index }) => setIndex(index)}
+          />
+          <Lightbox
+            slides={items}
+            open={index >= 0}
+            index={index}
+            close={() => setIndex(-1)}
+            // enable optional lightbox plugins
+            plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+          />
+          <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination justify-content-center mt-2"}
+            pageClassName={"page-item"}
+            pageLinkClassName={"page-link"}
+            previousClassName={"page-item"}
+            previousLinkClassName={"page-link"}
+            nextClassName={"page-item"}
+            nextLinkClassName={"page-link"}
+            breakClassName={"page-item"}
+            breakLinkClassName={"page-link"}
+            activeClassName={"active"}
+          />
+        </div>
       </div>
-      <PhotoAlbum
-        layout="rows"
-        photos={items}
-        containerWidth={900}
-        spacing={20}
-        padding={20}
-        targetRowHeight={200}
-        renderContainer={renderContainer}
-        renderPhoto={renderPhoto}
-        onClick={({ photo: { File }, index }) => setIndex(index)}
-      />
-      <Lightbox
-        slides={items}
-        open={index >= 0}
-        index={index}
-        close={() => setIndex(-1)}
-        // enable optional lightbox plugins
-        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
-      />
-      <ReactPaginate
-        previousLabel={"previous"}
-        nextLabel={"next"}
-        breakLabel={"..."}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={3}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination justify-content-center mt-2"}
-        pageClassName={"page-item"}
-        pageLinkClassName={"page-link"}
-        previousClassName={"page-item"}
-        previousLinkClassName={"page-link"}
-        nextClassName={"page-item"}
-        nextLinkClassName={"page-link"}
-        breakClassName={"page-item"}
-        breakLinkClassName={"page-link"}
-        activeClassName={"active"}
-      />
-    </div>
-</div>
-</Layout>
+    </Layout>
   );
 }
-export default Publications
+export default Publications;
 export async function getStaticProps() {
-  const res = await
-    fetch( `${API_URL}/api/publications?populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=10`)
+  const res = await fetch(
+    `${API_URL}/api/publications?populate=*&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=10`
+  );
 
-  const photos = await res.json()
+  const photos = await res.json();
   const total = photos.meta.pagination.total;
-  let mydata = new Array;
-  let myFileSrc
+  let mydata = new Array();
+  let myFileSrc;
   photos.data?.map((clip) => {
-    if( !clip.attributes.File)
-    {
-      myFileSrc = "No File"
-    }
-    else
-    {
-      myFileSrc=clip.attributes.File.data.attributes.url
+    if (!clip.attributes.File) {
+      myFileSrc = "No File";
+    } else {
+      myFileSrc = clip.attributes.File.data.attributes.url;
     }
     mydata.push({
-      "file": myFileSrc,
-      "src": clip.attributes.cover.data.attributes.url,
-      "width": clip.attributes.cover.data.attributes.width,
-      "height": clip.attributes.cover.data.attributes.height,
-      "images": [
-        { "src": clip.attributes.cover.data.attributes.url },
-        { "src": clip.attributes.cover.data.attributes.formats.thumbnail.url }
-      ]
+      title: clip.attributes.title,
+      file: myFileSrc,
+      src: clip.attributes.cover.data.attributes.url,
+      width: clip.attributes.cover.data.attributes.width,
+      height: clip.attributes.cover.data.attributes.height,
+      images: [
+        { src: clip.attributes.cover.data.attributes.url },
+        { src: clip.attributes.cover.data.attributes.formats.thumbnail.url },
+      ],
     });
   });
   return {
     props: {
       mydata,
-      total
+      total,
     },
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 10 seconds
     revalidate: 10, // In seconds
-  }
+  };
 }
