b279de091a060bb3048c89d4faa1e976eb397b89
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-03-14 00:08:16 +0500

    Change the next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www",
+          },
+        ],
+        destination: "/:path*",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
   },
 };
 
