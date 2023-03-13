c823f36b8d256b14d23c6e0e6012fd3c44edbae7
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-03-14 00:11:15 +0500

    Restore next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www",
-          },
-        ],
-        destination: "/:path*",
-        permanent: true,
-      },
-    ];
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
