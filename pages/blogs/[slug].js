c7b17d4b6675ecb2e5e32cac1379796ee68710e2
Amjad Iqbal Khan <amjad.iqbal@VIBD01.local>
2023-04-12 14:10:46 +0500

    Updated blogs

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,7 +1,9 @@
 import { API_URL } from "@/config/index";
 import ReactMarkdown from "react-markdown";
-const Blog = ({ data }) => {
-  // console.log(data)
+const BlogDetails = ({ data }) => {
+   //console.log(data)
+   //return false;
+   //consl
   return (
     <div className="wrapper ">
       {/* main content */}
@@ -11,17 +13,17 @@ const Blog = ({ data }) => {
             <div className=" h-auto col-md-12">
               <div className="block-title-6 text-center">
                 <h4 className="h5 border-primary ">
-                  <span className="bg-primary text-white">Post details</span>
+                  <span className="bg-primary text-white">Blog details</span>
                 </h4>
               </div>
 
               <div className="">
                 <div className="card border-0  p-3">
-                  <h3 className="card-title">{data[0].attributes.title}</h3>
+                  <h3 className="card-title">{data.data[0].attributes.title}</h3>
                   <div className="card-body">
                     <p>
                       <ReactMarkdown>
-                        {data[0].attributes.description}
+                        {data.data[0].attributes.description}
                       </ReactMarkdown>
                     </p>
                   </div>
@@ -34,38 +36,24 @@ const Blog = ({ data }) => {
     </div>
   );
 };
-export default Blog;
-export async function getStaticProps(context) {
-  const { params } = context;
+
+export async function getServerSideProps({ params }) {
+  const { slug } = params;
+
+  //fetching profiles
   const res = await fetch(
-    `${API_URL}/api/posts?filters[type][$eq]=Blog&filters[slider][$eq]=false&filters[slug][$eq]=${params.slug}`
+    `${API_URL}/api/posts?filters[type][$eq]=Blog&filters[slider][$eq]=false&filters[slug][$eq]=${slug}`
   );
   const posts = await res.json();
   const data = posts.data;
-
+  //fetching profiles
+  //console.log("Profiles", Profiles);
   return {
     props: {
-      data,
+      data: {
+        data,
+      },
     },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
   };
 }
-export async function getStaticPaths() {
-  const res = await fetch(
-    `${API_URL}/api/posts?filters[type][$eq]=Blog&filters[slider][$eq]=false`
-  );
-  const posts = await res.json();
-  const data = posts.data;
-  // Get the paths we want to pre-render based on posts
-  const paths = data.map((post) => {
-    return {
-      params: {
-        slug: `${post.attributes.slug}`,
-      },
-    };
-  });
-  return { paths, fallback: false };
-}
+export default BlogDetails;
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,62 +1,148 @@
+import React, { useState, useEffect } from "react";
 import { API_URL } from "@/config/index";
-import moment from "moment";
 import Link from "next/link";
+import ReactPaginate from "react-paginate";
 import ReactMarkdown from "react-markdown";
+import moment from "moment";
+import Image from "next/image";
 import Layout from "@/components/Layout";
-function Blogs({ data }) {
-  //  console.log("posts data", data)
-  // return false;
+const Blog = () => {
+  const [items, setItems] = useState([]);
+  const [pageCount, setpageCount] = useState(0);
+  let limit = 10;
+  useEffect(() => {
+    const getHighlights = async () => {
+      const res = await fetch(
+        `${API_URL}/api/posts?filters[type][$eq]=Blog&filters[slider][$eq]=false&sort=createdAt:desc&pagination[page]=1&pagination[pageSize]=${limit}`
+        // `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
+      );
+      const data = await res.json();
+      console.log("hightlights", data);
+      const total = data.meta.pagination.total;
+      setpageCount(Math.ceil(total / limit));
+      // console.log(Math.ceil(total/12));
+      setItems(data);
+    };
+
+    getHighlights();
+  }, [limit]);
+  // console.log("items", items);
+  const fetchPublications = async (currentPage) => {
+    const res = await fetch(
+      `${API_URL}/api/posts?filters[type][$eq]=Blog&filters[slider][$eq]=false&sort=createdAt:desc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
+      // `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
+    );
+    const data = await res.json();
+    return data;
+  };
+  const handlePageClick = async (data) => {
+    console.log(data.selected);
+
+    let currentPage = data.selected + 1;
+
+    const publicationFromServer = await fetchPublications(currentPage);
+
+    setItems(publicationFromServer);
+  };
   return (
-    <Layout title="Blogs">
+    <Layout title="Highlights">
       <div className="wrapper ">
         {/* main content */}
         <main id="content">
           <div className="container">
-            <div className="d-flex flex-row mt-3 p-3  mb-3">
-              <div className=" h-auto col-md-12">
+            <div className="row p-4 mt-20">
+              <div className="col-sm-12">
                 <div className="block-title-6 text-center">
-                  <h4 className="h5 border-primary ">
+                  <h4 className="h5 border-primary">
                     <span className="bg-primary text-white">Blogs</span>
                   </h4>
                 </div>
-                <div className="">
-                  {data.map((post) => (
-                    <div
-                      className="card border-0 border-bottom p-2"
-                      key={post.id}
-                    >
-                      <div className="card-body">
-                        <Link href={`blogs/${post.attributes.slug}`}>
-                          <h4 className="card-title" key={post.id}>
-                            {post.attributes.title}
-                          </h4>
-
-                          <p>
-                            {" "}
-                            <small>
-                              {" "}
-                              <time dateTime="2019-10-21">
-                                {moment(post.attributes.createdAt).format(
-                                  "Do MMMM YYYY"
-                                )}
-                              </time>
-                            </small>
-                          </p>
-                          <p
+                <div className="border-bottom-last-0 first-pt-0">
+                  {items &&
+                    items.data?.map((highlight) => {
+                      return (
+                        <article
+                          className="card card-full hover-a py-5"
+                          key={highlight.attributes.id}
+                        >
+                          <div className="row">
+                            <div className="col-sm-3">
+                              <div>
+                              {highlight.attributes.image > 0 &&
+                                <Image
+                                  width={
+                                    highlight.attributes.image.data[0]
+                                      .attributes?.formats.thumbnail.width
+                                  }
+                                  height={
+                                    highlight.attributes.image.data[0]
+                                      .attributes?.formats.thumbnail.height
+                                  }
+                                  src={
+                                    highlight.attributes.image.data[0]
+                                      .attributes.url
+                                  }
+                                  alt="Image description"
+                                />
+                               }
+                              </div>
+                            </div>
+                            <div className="col-sm-9 mt-3">
+                              <div className="card-body pt-3 pt-sm-0 pt-md-3 pt-lg-0">
+                              <Link href={`blogs/${highlight.attributes.slug}`}>
+                                  <h3 className="card-title h2 h3-sm h2-md">
+                                    {highlight.attributes.title}
+                                  </h3>
+                                </Link>
+                                <div className="card-text mb-2 text-muted small">
+                                  {/* <span className="d-none d-sm-inline me-1">
+                              <a className="fw-bold" href="#">
+                                John Doe
+                              </a>
+                            </span> */}
+                                  <time dateTime="2019-10-21">
+                                    {moment(
+                                      highlight.attributes.createdAt
+                                    ).format("Do MMMM YYYY")}
+                                  </time>
+                                </div>
+                                <p
                             className="card-text mt-3"
                             style={{ textAlign: "justify" }}
                           >
                             <ReactMarkdown>
-                              {post.attributes.description
+                              {highlight.attributes.description
                                 .substring(0, 300)
                                 .concat("...")}
                             </ReactMarkdown>
                           </p>
-                        </Link>
-                      </div>
-                    </div>
-                  ))}
+                              </div>
+                            </div>
+                          </div>
+                        </article>
+                      );
+                    })}
                 </div>
+
+                <ReactPaginate
+                  previousLabel={"previous"}
+                  nextLabel={"next"}
+                  breakLabel={"..."}
+                  pageCount={pageCount}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={3}
+                  onPageChange={handlePageClick}
+                  containerClassName={"pagination justify-content-center"}
+                  pageClassName={"page-item"}
+                  pageLinkClassName={"page-link"}
+                  previousClassName={"page-item"}
+                  previousLinkClassName={"page-link"}
+                  nextClassName={"page-item"}
+                  nextLinkClassName={"page-link"}
+                  breakClassName={"page-item"}
+                  breakLinkClassName={"page-link"}
+                  activeClassName={"active"}
+                />
               </div>
             </div>
           </div>
@@ -64,25 +150,30 @@ function Blogs({ data }) {
       </div>
     </Layout>
   );
-}
-export default Blogs;
+};
+
+export default Blog;
+
+
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
-export async function getStaticProps() {
-  const res = await fetch(
-    `${API_URL}/api/posts?filters[type][$eq]=Blog&filters[slider][$eq]=false&sort=createdAt:asc`
-  );
-  const posts = await res.json();
-  const data = posts.data;
 
-  return {
-    props: {
-      data,
-    },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
-  };
-}
+// export async function getServerSideProps() {
+//   //fetching thematic-area
+//   const thematicareares = await fetch(
+//     `${API_URL}/api/posts?filters[type][$eq]=Blog&filters[slider][$eq]=false&sort=createdAt:asc`
+//   );
+//   const posts = await thematicareares.json();
+ 
+//   //fetching Publications
+//   //  console.log("ThematicAreas", ThematicAreas);
+//   return {
+//     props: {
+//       data: {
+//         posts,
+//       },
+//     },
+//   };
+// }
+// export default Blogs;
