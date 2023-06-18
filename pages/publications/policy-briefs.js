084839a046c4fa2fabe2b4d0a5113350e4e618a3
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2023-06-18 14:53:23 +0500

    Added policy brief

--- pages/publications/policy-briefs.js
+++ pages/publications/policy-briefs.js
@@ -83,7 +83,7 @@ function Photosalbum(props) {
 
   const fetchPhotos = async (currentPage) => {
     const res = await fetch(
-      `${API_URL}/api/event-reports?populate=*&populate[0]=Report&populate[1]=Report.cover&populate[2]=Report.file&sort=rannk:asc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
+      `${API_URL}/api/policy-briefs?populate=*&populate[0]=Report&populate[1]=Report.cover&populate[2]=Report.file&sort=rannk:asc&pagination[page]=${currentPage}&pagination[pageSize]=${limit}`
     );
     const data = await res.json();
     let mydata = new Array();
@@ -175,7 +175,7 @@ function Photosalbum(props) {
 export default Photosalbum;
 export async function getStaticProps() {
   const res = await fetch(
-    `${API_URL}/api/event-reports?populate=*&populate[0]=Report&populate[1]=Report.cover&populate[2]=Report.file&sort=rannk:asc&pagination[page]=1&pagination[pageSize]=10`
+    `${API_URL}/api/policy-briefs?populate=*&populate[0]=Report&populate[1]=Report.cover&populate[2]=Report.file&sort=rannk:asc&pagination[page]=1&pagination[pageSize]=10`
   );
 
   const photos = await res.json();
